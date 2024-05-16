d2761ffa036d5195a0a85fd0b4464bab0e21e12f
Jelle van der Lee <Jellemvdl@gmail.com>
2024-05-16 19:23:17 +0200

    Add demo scripts and eval jobs

---
+++ src/scripts/inference_demos/iterative_generation/demo_iterative_r2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sh_file_name="demo_iterative_r2.sh"
+gpu="0"
+
+base_path="InstanceDiffusion-extension/src/lib/instancediffusion"
+
+python inference.py \
+  --num_images 8 \
+  --output OUTPUT/ \
+  --input_json ${base_path}/demos/demo_iterative_r2.json \
+  --ckpt ${base_path}/pretrained/instancediffusion_sd15.pth \
+  --test_config ${base_path}/configs/test_box.yaml \
+  --guidance_scale 7.5 \
+  --alpha 0.8 \
+  --seed 0 \
+  --mis 0.2 \
+  --cascade_strength 0.4 \
